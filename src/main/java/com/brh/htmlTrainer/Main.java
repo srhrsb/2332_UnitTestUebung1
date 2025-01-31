e0da018a73305d9efa944df28005fc96dc3fc7d5
srhrsb <sebastian.boettcher2@srh.de>
2025-01-31 10:30:43 +0100

    Uebung Tests

--- src/main/java/com/brh/htmlTrainer/Main.java
+++ src/main/java/com/brh/htmlTrainer/Main.java
@@ -1,5 +1,7 @@
 package com.brh.htmlTrainer;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -10,7 +12,6 @@ public class Main {
     }
 
     public static  int multiply (int v1, int v2){
-
         return v1 * v2;
     }
 
@@ -18,6 +19,14 @@ public class Main {
         return new Car();
     }
 
+    public static int[] getNumbersArray( int min, int max ){
 
+        int[] array = new int[max-min+1];
 
+        for(int i = 0; i < array.length ; i++){
+            array[i] = min+i;
+        }
+
+        return array;
+    }
 }
--- src/test/java/com/brh/htmlTrainer/MainTest.java
+++ src/test/java/com/brh/htmlTrainer/MainTest.java
@@ -11,18 +11,18 @@ class MainTest {
     @Test
     @DisplayName("Addition")
     void testSum() {
-        assertEquals( 10, Main.sum(4, 6), "Addition korrekt" );
+        assertEquals(10, Main.sum(4, 6), "Addition korrekt");
     }
 
     @Test
     void testMultiply() {
-        assertEquals( 12, Main.multiply(2, 6), "Multiplikation korrekt" );
+        assertEquals(12, Main.multiply(2, 6), "Multiplikation korrekt");
     }
 
     @RepeatedTest(5)
-    void testMultiplyZero(){
-        assertEquals( 0, Main.multiply(0,5), "Multiplikation mit 0");
-        assertEquals( 0, Main.multiply(5,0), "Multiplikation mit 0");
+    void testMultiplyZero() {
+        assertEquals(0, Main.multiply(0, 5), "Multiplikation mit 0");
+        assertEquals(0, Main.multiply(5, 0), "Multiplikation mit 0");
     }
 
 
@@ -32,5 +32,16 @@ class MainTest {
         Car car = Main.getNewCar();
         assertNotNull(car, "Nicht ungueltig");
 
+    }
+
+    @Test
+    void testGetNumbersArray() {
+
+        int[] numbers = Main.getNumbersArray(2, 5);
+
+        assertEquals(2, numbers[0], "Nummer korrekt");
+        assertEquals(3, numbers[1], "Nummer korrekt");
+        assertEquals(4, numbers[2], "Nummer korrekt");
+        assertEquals(5, numbers[3], "Nummer korrekt");
     }
 }
